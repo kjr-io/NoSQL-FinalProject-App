4fabb62d10329d02356f3a83b9a9bc803c0cd17a
kylejohnyan <kyle.ryan2@marist.edu>
2021-11-08 14:57:06 -0500

    Updates

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+import pymongo
 
 app = Flask(__name__)
+try:
+    mongo = pymongo.MongoClient(
+        host = "localhost",
+        port = 27017,
+        serverSelectionTimeoutMS = 1000
+    )
+
+    # We Will Need to Create movies DB in Mongo Compass
+    db = mongo.movies
+    # Triggers Exception
+    mongo.server_info()
+except:
+    print ("ERROR -- Cannot Connect to the DB.")
 
 @app.route("/")
 def homepage():
     return "<p> Hello, World! </p>"
+
+if __name__ == "__main__":
+    app.run(port = 8080, debug = True)
