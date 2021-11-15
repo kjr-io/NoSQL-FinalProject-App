558a471208f857d3c1dc5c808d0d28f248f8fdae
kylejohnyan <kyle.ryan2@marist.edu>
2021-11-15 14:27:01 -0500

    Updates

--- pipeline/data-insert.py
+++ pipeline/data-insert.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from flask_pymongo import PyMongo
 
+client = PyMongo.MongoClient("mongodb://localhost:27017")
+
 def csv_to_json(filename, header=None):
     data = pd.read_csv(filename, header = header)
     return data.to_dict('movies')
 
-collection.insert_many(csv_to_json('your_file_path'))
+movies.insert_many(csv_to_json('your_file_path'))
+
