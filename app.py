c4845a3e989b1ebfb22ddd2b68e3e6e54ba506c5
kylejohnyan <kyle.ryan2@marist.edu>
2021-12-10 14:05:48 -0500

    Complete

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ except:
 
 # CRUD Routes
 # ---------------------------------------------------------------- #
-
 # Log in
 @app.route("/", methods=["POST", "GET"])
 def login():
@@ -71,6 +70,8 @@ def login():
 @app.route('/logged_in', methods=["POST", "GET"])
 def logged_in():
     if request.method == "POST":
+
+        # Looking for Requested Movie from Form, Passing to Mongo
         movie = request.form.get("movie")
         movie_found = db.movies.find_one({"title": movie})
         if movie_found:
@@ -81,12 +82,12 @@ def logged_in():
             session['plot'] = movie_found['fullplot']
             session['awards'] = movie_found['awards_wins']
 
+            # This Appears Like a List but is Actually Just One String
+            # Therefore to Output Correctly, the Data Must be Stripped & Replaced
             cast = movie_found['cast']
             castStripped = cast.strip("[]")
             session['cast'] = castStripped.replace('"', '')
 
-            # This Appears Like a List but is Actually Just One String
-            # Therefore to Output Correctly, the Data Must be Stripped & Replaced
             countries = movie_found['countries']
             countriesStripped = countries.strip("[]")
             session['countries'] = countriesStripped.replace('"', '')
@@ -99,6 +100,7 @@ def logged_in():
             genresStripped = genres.strip("[]")
             session['genres'] = genresStripped.replace('"', '')
 
+            # Gathering Other Information Useful to the User 
             session['imdb_rating'] = movie_found['imdb_rating']
             session['imdb_votes'] = movie_found['imdb_votes']
             session['rated'] = movie_found['rated']
@@ -111,11 +113,12 @@ def logged_in():
                 # Gathering Number of Comments to Iterate On if the User Comments
                 session['num_comments'] = movie_found['num_mflix_comments']
 
+                # If num_mflix_comments is NaN, Set it to Zero
                 if int(session['num_comments']) < 0:
                     db.movies.update_one({'_id': session['_id']}, {"$set": {"num_mflix_comments": "0"}})
                 
             except:
-                # If There are No Comments, Add the Column and Set to Zero
+                # If There are No Comments Field, Add the Field and Set to Zero
                 db.movies.update_one({'_id': session['_id']}, {"$set": {"num_mflix_comments": "0"}})
 
             return redirect(url_for('movie_query'))
@@ -125,12 +128,20 @@ def logged_in():
     return render_template('logged_in.html', name=currentUser)
 
 # Returning Movie Results
+# Stored Outside of a Route so Delete Can Access the List
 commentIdList = []
 @app.route('/movie_query_results', methods=["POST", "GET"])
 def movie_query():
     # Query to Find Comments Associated with the Movie the User Searched
     # Trying to Find Multiple Comments Here
     commentsList = []
+
+    # Clearing the List Whenever Page is Refreshed, Prevents Duplicates Since List is Outside the Route
+    commentIdList.clear()
+
+    # Finding All of the Comments and Appending to a List
+    # commentsList is to be Rendered on the Page, Best Way to Pass Many to Jinja
+    # commentIdList is so We Can Identify Which Comment to Delete by the _id
     for doc in db.comments.find({"movie_id": session["_id"]}):
         commentsList.append(doc['text'] + ' by ' + doc['name'])
         commentIdList.append(doc['_id'])
@@ -139,6 +150,7 @@ def movie_query():
     # User Comments if POST Request Received
     if request.method == "POST":
         if 'usercomment' in request.form:
+            # Gather All Information for New Comment Document and Insert it
             comment = {
                 "date":datetime.datetime.utcnow(),
                 "email":session["email"],
@@ -151,6 +163,7 @@ def movie_query():
 
             # If the User Comments, We Have to Update Num Comments
             try:
+                # Grabs as Int, Iterates by One, Stores it Back as a String (Data Type in the DB)
                 num_commentsInt = int(session['num_comments'])
                 num_commentsInt += 1
                 db.movies.update_one (
@@ -160,6 +173,7 @@ def movie_query():
                 print(' * Counter Successfully Updated!')
             except:
                 print(' * Counter Failed.')
+                
             # If the User Comments, Redirect Them Back to the Page to Show the New Comment
             return redirect(url_for('movie_query'))
         if 'userrating' in request.form:
@@ -167,18 +181,21 @@ def movie_query():
                 currentRating = session['imdb_rating']
                 updatedVotes = session['imdb_votes']
 
+                # Gathering the User Rating and Recalculating the Average
                 userRatingForm = int(request.form["userrating"])
                 updatedRatings = round((((currentRating * updatedVotes) + userRatingForm)/(updatedVotes + 1)), 2)
 
+                # Iterating Votes
                 updatedVotes += 1
-                
+
+                # Update Votes and Rating using update_many
                 db.movies.update_many (
                     {"_id": session["_id"]},
                     {"$set": {"imdb_votes": updatedVotes, "imdb_rating": updatedRatings}}
                 )
                 print(' * Counter Successfully Updated!')
                 print(' * Rating Successfully Updated!')
-                
+                return redirect(url_for('movie_query'))
             except:
                 print(' * Counter & Rating Failed.')
             return redirect(url_for('movie_query'))
@@ -204,14 +221,20 @@ def movie_query():
 @app.route('/delete/<index>', methods=["GET", "POST"])
 def delete(index):
     try:
+        # Creating var Based off of Jinja Index (loop.index0) Which Helps us Determine Which Comment is What
         var = int(index)
+        # Retrieving this Comment's _id in the commentIdList
         objId = str(commentIdList[var])
-        dbResponse = db.comments.delete_one({"_id": ObjectId(objId)})
+        try:
+            # If User Made Comment
+            dbResponse = db.comments.delete_one({"_id": ObjectId(objId)})
+        except:
+            # If Comment Previously Existed in the DB
+            dbResponse = db.comments.delete_one({"_id": objId})
         if dbResponse.deleted_count == 1:
             print(' * Comment Deleted.')
         else: 
             print('* Comment not Found.')
-        commentIdList.clear()
         return redirect(url_for('movie_query'))
     except:
         print('Cannot Delete Comment.')
@@ -225,63 +248,6 @@ def logout():
         return render_template("signout.html")
     else:
         return render_template('login.html')
-
-# CRUD Operation Examples
-# ---------------------------------------------------------------- #
-
-# Create Document Routes
-@app.route("/users", methods=["POST"])
-def create_user():
-    try:
-        user = {
-            "email":request.form["email"],
-            "name":request.form["name"],
-            "password":request.form["password"]
-        }
-        dbResponse = db.users.insert_one(user)
-        print(dbResponse.inserted_id)
-    except Exception as ex:
-        print(ex)
-
-# Read Document Routes
-@app.route("/users", methods=["GET"])
-def get_some_user():
-    try:
-        data = list(db.users.find())
-        print(data)
-        for user in data:
-            user["_id"] = str(user["_id"])
-    except Exception as ex:
-        print(ex)
-
-# Update Document Routes
-@app.route("/users/<id>", methods=["PATCH"])
-def update_user(id):
-    try:
-        dbResponse = db.users.update_one(
-            {"_id": ObjectId(id)},
-            {"$set": {"name": request.form["name"]}}
-        )
-        if dbResponse.modified_count == 1:
-            print('Updated.')
-        else:
-            print('Nothing to Update')
-    except Exception as ex:
-        print('Sorry Cannot Update')
-
-
-# Delete Document Routes
-@app.route("/users/<id>", methods=["DELETE"])
-def delete_user(id):
-    try:
-        dbResponse = db.users.delete_one({"_id": ObjectId(id)})
-        if dbResponse.deleted_count == 1:
-            print('User Deleted')
-        else: 
-            print('User not Found')
-    except Exception as ex:
-        print('Cannot Delete User')
-
 
 # Starting the Web Server
 # ---------------------------------------------------------------- #
